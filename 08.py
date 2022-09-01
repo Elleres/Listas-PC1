9dd1296ff17c73cb3d07dcccf2f212684545167b
Arthur Elleres <73797131+Elleres@users.noreply.github.com>
2022-09-01 09:58:58 -0300

    Delete 08.py

--- 08.py
+++
@@ -1,3 +0,0 @@
-sal_hora = float(input("Digite quanto você recebe por hora: "))
-horas_mes = int(input("Digite quantas horas você trabalha por mês: "))
-print(f"Seu salário mensal é de: {sal_hora*horas_mes}")
