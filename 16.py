87bb3aada0f58f45196ab605443ae910aa866881
Arthur Elleres <73797131+Elleres@users.noreply.github.com>
2022-09-01 09:56:16 -0300

    Delete 16.py

--- 16.py
+++
@@ -1,8 +0,0 @@
-from math import ceil
-
-
-m2 = float(input("Digite quantos metros quadrados deseja pintar: "))
-qnt_litros = m2 / 3
-qnt_latas = qnt_litros / 18
-print(f"Para pintar {m2} m2 será necessário {qnt_litros:.2f} litros o sendo então necessário comprar {ceil(qnt_latas)} latas"
-      f" resultando em R${ceil(qnt_latas) * 80}.")
