9c3b320fc58047301b22478974ea2e0f6779f6ef
Arthur Elleres <73797131+Elleres@users.noreply.github.com>
2022-09-01 09:56:46 -0300

    Delete 14.py

--- 14.py
+++
@@ -1,7 +0,0 @@
-peso_peixe = float(input("Digite o peso do peixe: "))
-if peso_peixe > 50:
-    excesso = peso_peixe - 50
-    multa = excesso * 4
-    print(
-        f"O peso do peixe é de {peso_peixe} com excesso de {excesso} e é necessário pagar multa de R${multa}")
-print(f"O peso do peixe é de {peso_peixe} e não é necessário pagar multa!")
