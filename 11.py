d51faa00ea78931937f24549904220117de4ba54
Arthur Elleres <73797131+Elleres@users.noreply.github.com>
2022-09-01 09:58:26 -0300

    Delete 11.py

--- 11.py
+++
@@ -1,4 +0,0 @@
-ni1 = int(input("Digite o primeiro número inteiro: "))
-ni2 = int(input("Digite o segundo número inteiro: "))
-nreal = float(input("Digite um número real: "))
-print(f"a) {(2*ni1)*(ni2/2)}\nb){(3*ni1)+(nreal)}\nc){nreal**3}")
