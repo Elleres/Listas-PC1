3eaa29fe1d83c140d1c63bbe854fe38f57421a42
Arthur Elleres <73797131+Elleres@users.noreply.github.com>
2022-09-01 09:57:33 -0300

    Delete 13.py

--- 13.py
+++
@@ -1,6 +0,0 @@
-h = float(input("Digite a sua altura: "))
-s = input("H: homem; M: mulher.")
-if s in "hH":
-    print(f"Seu peso ideal é de: {72.7*h - 58}")
-elif s in "mM":
-    print(f"Seu peso ideal é de: {62.1 * h - 44.7}")
